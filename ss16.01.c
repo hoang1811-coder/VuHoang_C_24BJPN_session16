9cbcace80048b9f9f5f4c24a19b6e8f2d5fe4c63
hoang1811-coder <vhoangssz1@gmail.com>
2024-12-18 13:45:51 +0700

    Add files via upload

---
+++ ss16.01.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int numbers=20;
+	int *numbersPtr=&numbers;
+	printf("gia tri cua bien numbers = %d\n", &numbers);
+	printf("dia chi cua numbersPtr = %lu", &numberPtr);
+	
+	
+	
+	return 0;
+}
